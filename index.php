afbf645372833251ee58f9bc2354c7613a7ba01a
TheHasax <TheHasax@DESKTOP-6FD5QCO>
2017-06-01 16:10:12 +0200

    Merge origin/master
    Conflicts:
    	css/custom.css
    	index.php
    	nbproject/project.properties
    	nbproject/project.xml

--- index.php
+++ index.php
@@ -8,9 +8,10 @@ and open the template in the editor.
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
-        <title>Rigiet</title>
+        <title>Rigiet - Optag dit liv som aldrig før</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/custom.css">
+        <link href='//fonts.googleapis.com/css?family=Abel|Open+Sans' rel='stylesheet' type='text/css'>
     </head>
     <body >
         <div id="top_header" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -19,18 +20,22 @@ and open the template in the editor.
         </div>
         <header>
             <video playsinline center autoplay muted loop poster="images/backup.jpg" id="bgvid">
-                <source src="video/header.webm" type="video/webm">
-                <source src="video/header.mp4" type="video/mp4">
+                <source src="video/header.webm" type="video/webm" align="middle">
+                <source src="video/header.mp4" type="video/mp4" align="middle">
             </video>
             <div id="header_info" class="pull-right col-lg-4 col-md-5 col-sm-6">
-                <h1>Rigiet</h1>
+                <p class="h1">Rigiet</p>
                 <p>
                 <p><a href="/777/Create-Fullscreen-HTML5-Page-Background-Video">Return to article</a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta dictum turpis, eu mollis justo gravida ac. Proin non eros blandit, rutrum est a, cursus quam. Nam ultricies, velit ac suscipit vehicula, turpis eros sollicitudin lacus, at convallis mauris magna non justo. Etiam et suscipit elit. Morbi eu ornare nulla, sit amet ornare est. Sed vehicula ipsum a mattis dapibus. Etiam volutpat vel enim at auctor.
+                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta dictum turpis, eu mollis justo gravida ac. Proin non eros blandit, rutrum est a, cursus quam. Nam ultricies, velit ac suscipit vehicula, turpis eros sollicitudin lacus, at convallis mauris magna non justo. Etiam et suscipit elit. Morbi eu ornare nulla, sit amet ornare est. Sed vehicula ipsum a mattis dapibus. Etiam volutpat vel enim at auctor.</h1>
                 <p><a class="col-lg-12 col-md-12 col-sm-12" href="signup.php">Signup</a>
             </div>
         </header>
+        
         <div id="wrapper" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div id="big_logo" class="container">
+                <img src="images/Rigiet Logo_1.png" alt="" align="middle"/>
+            </div>
             <div class="filler col-lg-1 col-md-1"><img src="images/blank_br.png" alt=""/></div>
             
             <div id="info_1" class="test_right pull-right col-lg-4 col-md-4 col-sm-6 col-xs-12">
@@ -38,7 +43,8 @@ and open the template in the editor.
                 <div class="test_blank_mb"><img src="images/blank-mb.png" alt=""/></div>
             </div>
             <div class="test_text_left pull-left col-lg-5 col-md-5 col-sm-6 col-xs-12">
-                Lorem ipsum blahLorem ipsum blahLorem ipsum blahLorem ipsum blahLorem ipsum blahLorem ipsum blahLorem ipsum blah
+                <h3>Auto Tracking</h3>
+                <p>Rigiet kan automatisk følge bevægene objekter og personer og giver den smidige panning der gør din film mere professionel ligne.
             </div>
             <div class="filler col-lg-2 col-md-2"><img src="images/blank_br.png" alt=""/></div>
             <div class="space col-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
@@ -47,17 +53,44 @@ and open the template in the editor.
                  <div class="test_blank"><img src="images/blank_br.png" alt=""/></div>
                  <div class="test_blank_mb"><img src="images/blank-mb.png" alt=""/></div>
             </div>
+            <div class="filler col-lg-1 col-md-1"><img src="images/blank_br.png" alt=""/></div>
             <div class="test_text_right col-lg-5 col-md-5 col-sm-6 col-xs-12">
-                Lorem ipsum blahLorem ipsum blahLorem ipsum blahLorem ipsum blahLorem ipsum blahLorem ipsum blahLorem ipsum blah
+                <h3>Stabil kamera</h3>
+                <p>Syv forskellige optagelsestilstande dækker enhver situation i din telefonfilm. Uanset om du vil filme i landskabs-, portræt- eller underposition, kan Rigiet automatisk registrere og justere til den position, du vil filme i.
             </div>
             <div class="filler col-lg-1 col-md-1"><img src="images/blank_br.png" alt=""/></div>
-            
+            <div class="space col-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
+            <div class="filler col-lg-1 col-md-1"><img src="images/blank_br.png" alt=""/></div>
+            <div id="info_3" class="test_right pull-right col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="test_blank"><img src="images/blank_tl.png" alt=""/></div>
+                <div class="test_blank_mb"><img src="images/blank-mb.png" alt=""/></div>
+            </div>
+            <div class="test_text_left pull-left col-lg-5 col-md-5 col-sm-6 col-xs-12">
+                <h3>Levende panorama</h3>
+                <p>Med Rigiet, kan du skabe dit egen levende panorama, der giver mere liv i dine omgivelser.
+            </div>
+            <div class="filler col-lg-2 col-md-2"><img src="images/blank_br.png" alt=""/></div>
+            <div class="space col-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
+            <div class="filler col-lg-2 col-md-2"><img src="images/blank_br.png" alt=""/></div>
+            <div id="info_4" class="test_left col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                 <div class="test_blank"><img src="images/blank_br.png" alt=""/></div>
+                 <div class="test_blank_mb"><img src="images/blank-mb.png" alt=""/></div>
+            </div>
+            <div class="filler col-lg-1 col-md-1"><img src="images/blank_br.png" alt=""/></div>
+            <div class="test_text_right col-lg-5 col-md-5 col-sm-6 col-xs-12">
+                <h3>Tilbehøret til dig</h3>
+                <p>Rigiet er det perfekte tilbehør til din Android telefon, iPhone og GoPro. Den er kompatibelt med alle kamerastativer.
+            </div>
+            <div class="filler col-lg-1 col-md-1"><img src="images/blank_br.png" alt=""/></div>
+            <div class="space col-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
+            <div class="space col-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
             <div class="filler1 col-lg-12 col-md-12 col-sm-12 col-xs-12"><img src="images/blank_br.png" alt=""/></div>
             <div class="filler col-lg-4 col-md-2 col-sm-2 col-xs-2"><img src="images/blank_br.png" alt=""/></div>
+            
             <div class="navBtn col-lg-4 col-md-8 col-sm-8 col-xs-8"><p><a class="col-lg-12 col-md-12 col-sm-12 col-xs-12" href="signup.php">Signup</a></div>
             <div class="footer col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <p class="pull-left">Hello world.<br>This is who we are</p>
-                <p class="pull-right">Contact us here:<br>name@domain.dk</p>
+                <p class="footleft pull-left">Hello world.<br>This is who we are</p>
+                <p class="footright pull-right">Contact us here:<br>name@domain.dk</p>
             </div>
         </div>
         
